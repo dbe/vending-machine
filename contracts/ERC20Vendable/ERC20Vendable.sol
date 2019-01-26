4c41f26eabcca06a5c1d6921188bd08a2fa6b4d5
dbe <davidbrianethier@gmail.com>
2019-01-25 19:08:06 -0700

    Adding changes to ERC20Vendable to be independent

--- contracts/ERC20Vendable/ERC20Vendable.sol
+++ contracts/ERC20Vendable/ERC20Vendable.sol
@@ -1,22 +1,30 @@
 pragma solidity 0.4.25;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract ERC20Vendable is ERC20 {
-  address public creator;
+contract ERC20Vendable is ERC20, Ownable {
+  address public vendingMachine;
   string public name;
   string public symbol;
   uint8 public decimals = 18;
 
   constructor(string memory _name, string memory _symbol) public {
-      creator = msg.sender;
-      name = _name;
-      symbol = _symbol;
+    name = _name;
+    symbol = _symbol;
   }
 
-  modifier onlyCreator() {
-    require(msg.sender == creator);
+  modifier onlyVendingMachine() {
+    require(msg.sender == vendingMachine);
     _;
+  }
+
+  //Ideally the Vending Machine would actually create the ERC20Vendable,
+  //and we could make mint and burn onlyCreator.
+  //However, to get the ERC20 contract deployed before we have all of the VendingMachine stuff ready,
+  //the ability to change is required to allow us to keep iterating on the VendingMachine design
+  function changeVendingMachine(address newVendingMachine) onlyOwner {
+    vendingMachine = newVendingMachine;
   }
 
   /**
@@ -25,7 +33,7 @@ contract ERC20Vendable is ERC20 {
    * @param amount The amount of tokens to mint.
    * @return A boolean that indicates if the operation was successful.
    */
-  function mint(address to, uint256 amount) public onlyCreator returns (bool) {
+  function mint(address to, uint256 amount) public onlyVendingMachine returns (bool) {
     _mint(to, amount);
     return true;
   }
@@ -38,7 +46,7 @@ contract ERC20Vendable is ERC20 {
    * @param from The address of which tokens should be burned away from.
    * @param value The amount of token to be burned.
    */
-  function burn(address from, uint256 value) public onlyCreator returns (bool) {
+  function burn(address from, uint256 value) public onlyVendingMachine returns (bool) {
     _burn(from, value);
     return true;
   }
